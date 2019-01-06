aa755e5781ce3a79366d59493c5b580d3ec43207
Vedant Kashyap <smartvedi@gmail.com>
2019-01-06 10:28:31 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Provider} from 'react-redux';
+
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+import store from "./store";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="App">
+                    <PostForm/>
+                    <hr/>
+                    <Posts/>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Postform.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {createPost} from '../actions/postActions';
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            body: ''
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        };
+
+        // Call Action
+        this.props.createPost(post);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Add Post</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Title: </label><br/>
+                        <input type="text" name="title" value={this.state.title} onChange={this.onChange}/>
+                    </div>
+                    <br/>
+                    <div>
+                        <label>Body: </label><br/>
+                        <textarea name="body" value={this.state.body} onChange={this.onChange}/>
+                    </div>
+                    <br/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+PostForm.propTypes = {
+    createPost: PropTypes.func.isRequired
+};
+
+export default connect(null, {createPost})(PostForm);
